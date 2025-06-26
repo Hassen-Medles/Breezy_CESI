c7670cb98c58c9478cfc52a09e033a66f301c476
Hassen-Medles <hassenyacine.medles@viacesi.fr>
2025-06-26 09:57:51 +0200

    modified:   backend/auth-service/src/models/user.model.js

--- backend/auth-service/src/models/user.model.js
+++ backend/auth-service/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { resolveContent } from "nodemailer/lib/shared";
+
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: false, unique: true },
